d190771bb74b47f59c9f4967ffa34920c04739bc
Clément Prévost <clement@rzb.fr>
2022-12-25 21:50:22 +0100

    cleanup

--- src/main/java/com/prevostc/adventofcode/Day21.java
+++ src/main/java/com/prevostc/adventofcode/Day21.java
@@ -2,16 +2,13 @@ package com.prevostc.adventofcode;
 
 import java.io.IOException;
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
+import com.prevostc.utils.BigInt;
 import com.prevostc.utils.Either;
 import com.prevostc.utils.FileReader;
-import com.prevostc.utils.Grid2d;
-import com.prevostc.utils.Vec2d;
 
 import lombok.val;
 
@@ -32,22 +29,18 @@ public class Day21 {
         // this never changes
         val right = solve(rootOp.rightId);
 
-        // when humn increases, solve(right) decreases
+        // when humn increases, solve(right) decreases so we can only search one
         var min = BigInteger.ZERO;
-        var max = BigInteger.valueOf(10).pow(15); // a previous attempt told us it's < 10^14
-        val two = BigInteger.valueOf(2);
+        var max = BigInt.ofPow(10, 15); // a previous attempt told us it's < 10^14
+        val two = BigInt.of(2);
         BigInteger found = null;
         while (min.compareTo(max) < 0) {
             val mid = min.add(max).divide(two);
             operators.put("humn", new Either.Right<>(mid));
             val left = solve(rootOp.leftId);
-            System.out.println(
-                    "humn: " + mid + " -> " + left + " " + rootOp.op + " " + right + " " + solve(rootOp.rightId));
             val cmp = left.compareTo(right);
 
             if (cmp == 0) {
-                System.out.println("Found solution: " + mid);
-                System.out.println(solve(rootOp.leftId) + " = " + solve(rootOp.rightId));
                 found = mid;
                 break;
             } else if (cmp * dir < 0) {
@@ -57,7 +50,8 @@ public class Day21 {
             }
         }
 
-        // find all solutions "around" the one we found
+        // find all solutions "around" the one we found due to division loss of
+        // precision
         // it's probably the lowest one
         for (int i = -100; i < 100; i++) {
             val humn = found.add(BigInteger.valueOf(i));
@@ -65,7 +59,6 @@ public class Day21 {
             val left = solve(rootOp.leftId);
             val cmp = left.compareTo(right);
             if (cmp == 0) {
-                System.out.println("Found solution: " + humn);
                 return humn;
             }
         }
@@ -83,18 +76,13 @@ public class Day21 {
             val op = oop.left();
             val left = solve(op.leftId);
             val right = solve(op.rightId);
-            switch (op.op) {
-                case '+':
-                    return left.add(right);
-                case '-':
-                    return left.subtract(right);
-                case '*':
-                    return left.multiply(right);
-                case '/':
-                    return left.divide(right);
-                default:
-                    throw new IllegalStateException("Unexpected value: " + op.op);
-            }
+            return switch (op.op) {
+                case '+' -> left.add(right);
+                case '-' -> left.subtract(right);
+                case '*' -> left.multiply(right);
+                case '/' -> left.divide(right);
+                default -> throw new IllegalStateException("Unexpected value: " + op.op);
+            };
         } else {
             return oop.right();
         }
@@ -115,7 +103,7 @@ public class Day21 {
                 val operator = matcher.group("operator");
                 val right = matcher.group("right");
                 if (operator == null) {
-                    operators.put(id, new Either.Right<>(BigInteger.valueOf(Integer.parseInt(left))));
+                    operators.put(id, new Either.Right<>(BigInt.of(left)));
                 } else {
                     operators.put(id, new Either.Left<>(new Operator(id, left, right, operator.charAt(0))));
                 }
--- src/test/java/com/prevostc/adventofcode/Day21Test.java
+++ src/test/java/com/prevostc/adventofcode/Day21Test.java
@@ -3,9 +3,10 @@ package com.prevostc.adventofcode;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
-import java.math.BigInteger;
 
 import org.junit.jupiter.api.Test;
+
+import com.prevostc.utils.BigInt;
 
 import lombok.val;
 
@@ -15,13 +16,14 @@ public class Day21Test {
     public void testPart1Example() throws IOException {
         val day = new Day21();
         val answer = day.part1("day21/example.txt");
-        assertEquals(BigInteger.valueOf(152), answer);
+        assertEquals(BigInt.of(152), answer);
     }
 
     @Test
     public void testPart1Main() throws IOException {
         val day = new Day21();
         val answer = day.part1("day21/input.txt");
+        assertEquals(BigInt.of("194501589693264"), answer);
         System.out.println("Day21 - Part 1: " + answer);
     }
 
@@ -29,13 +31,14 @@ public class Day21Test {
     public void testPart2Example() throws IOException {
         val day = new Day21();
         val answer = day.part2("day21/example.txt", 1);
-        assertEquals(BigInteger.valueOf(301), answer);
+        assertEquals(BigInt.of(301), answer);
     }
 
     @Test
     public void testPart2Main() throws IOException {
         val day = new Day21();
         val answer = day.part2("day21/input.txt", -1);
+        assertEquals(BigInt.of("3887609741189"), answer);
         System.out.println("Day21 - Part 2: " + answer);
     }
 }
